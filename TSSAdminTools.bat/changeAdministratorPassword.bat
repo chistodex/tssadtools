70c490a22f18ceceef5169c02e6ed862bc25db90
chistodex <cmitchell4@student.yosemite.edu>
2020-08-30 14:45:58 -0700

    Update changeAdministratorPassword.bat
    Removed hard coded password.

--- TSSAdminTools.bat/changeAdministratorPassword.bat
+++ TSSAdminTools.bat/changeAdministratorPassword.bat
@@ -7,7 +7,7 @@ echo activating local administrator...
 net user administrator /active:yes
 echo.
 echo setting local administrator account password...
-net user administrator @cme2570
+net user administrator password
 echo.
 echo setting local admin account password
 net user admin @cme2570
